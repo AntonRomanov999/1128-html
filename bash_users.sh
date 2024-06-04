1f5911b576ed3bced5015562cfcd1fdf32ef0497
AntonRomanov999 <155020101+AntonRomanov999@users.noreply.github.com>
2024-06-04 10:12:16 +0300

    Update bash_users.sh

--- bash_users.sh
+++ bash_users.sh
@@ -2,7 +2,7 @@
 echo "Let's search for users who use bash shell..."
 users=$(grep /bin/bash /etc/passwd)
 users_number=$(grep /bin/bash /etc/passwd|wc -l)
-echo "There are $users_number users:"
+echo "$users_number user(s) were found:"
 for line in $users
 do 
 	echo -e ${line%%:*}
